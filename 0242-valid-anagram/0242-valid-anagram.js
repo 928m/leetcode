7e9e5f1c98987c51f9fa9e1ea39ce539318dee06
928m <jihee9289@gmail.com>
2024-03-05 12:14:44 +0900

    Time: 66 ms (76.19%), Space: 54.3 MB (16.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -1,26 +1,10 @@
-function countedAlphabet(str) {
-  const obj = {};
-
-  for (const item of str.split("")) {
-    obj[item] = obj[item] ? obj[item] + 1 : 1;
-  }
-
-  return obj;
-}
-
 var isAnagram = function (s, t) {
   if (s.length !== t.length) {
     return false;
   }
 
-  const originObj = countedAlphabet(s);
-  const targetObj = countedAlphabet(t);
+  s = s.split("").sort().join("");
+  t = t.split("").sort().join("");
 
-  for (const key in originObj) {
-    if (originObj[key] !== targetObj[key]) {
-      return false;
-    }
-  }
-
-  return true;
+  return s === t;
 };

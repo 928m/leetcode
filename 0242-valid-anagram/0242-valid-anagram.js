533dd61139383812cfdfea4c237a485520dbcc70
928m <jihee9289@gmail.com>
2024-03-05 12:13:43 +0900

    Time: 92 ms (10.67%), Space: 52.8 MB (44.68%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,10 @@
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  s = s.split("").sort().join("");
+  t = t.split("").sort().join("");
+
+  return s === t;
+};

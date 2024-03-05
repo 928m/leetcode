394c91f3faa3b6f05f175079e828d902bb287e79
928m <jihee9289@gmail.com>
2024-03-05 12:39:17 +0900

    Time: 101 ms (33.4%), Space: 49.3 MB (61.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,19 +1,9 @@
-var twoSum = function (nums, target) {
-  const obj = {};
-
-  for (let idx = 0; idx < nums.length; idx += 1) {
-    const value = nums[idx];
-
-    obj[value] = idx;
-  }
-
-  for (let i = 0; i < nums.length; i += 1) {
-    const subtraction = target - nums[i];
-
-    if (obj[subtraction] && obj[subtraction] !== i) {
-      console.log([i, obj[subtraction]]);
-
-      return [i, obj[subtraction]];
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i += 1) {
+        for (let j = i + 1; j < nums.length; j += 1) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
     }
-  }
 };

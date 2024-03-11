9b3b3f4f19fe58fa9d41b0d447fd564f92b66f1e
928m <jihee9289@gmail.com>
2024-03-11 21:31:59 +0900

    Time: 186 ms (5.03%), Space: 58.2 MB (6.93%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -1,14 +1,23 @@
 var isPalindrome = function (s) {
   const string = s.toLowerCase().replace(/[^a-z0-9]/g, "");
   const half = string.length / 2;
+  let frontPointer = 0;
+  let backPointer = string.length - 1;
 
   if (string.length === 1) {
-      return true;
+    return true;
   }
 
-  return (
-    string.slice(0, half) === string.slice(-half).split("").reverse().join("")
-  );
+  while (frontPointer < half) {
+    console.log(string[frontPointer], string[backPointer]);
+    if (string[frontPointer] !== string[backPointer]) {
+      return false;
+    }
+    frontPointer++;
+    backPointer--;
+  }
+
+  return true;
 };
 
 isPalindrome("A man, a plan, a canal: Panama");

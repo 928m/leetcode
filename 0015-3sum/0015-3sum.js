05c2761a76c2a67c48da32e1de6d9feb73456daf
928m <jihee9289@gmail.com>
2024-03-25 20:39:54 +0900

    Time: 231 ms (19.76%), Space: 69.1 MB (5.42%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -1,35 +1,37 @@
 var threeSum = function(nums) {
     let main = 0;
-    let left = main + 1;
-    let right = left + 1;
     const obj = {};
 
     nums = nums.sort((a, b) => a - b);
 
-    while (main <= nums.length - 3) {
-        if (nums[main] + nums[left] + nums[right] === 0) {
-            const key = [nums[main], nums[left], nums[right]].join(',');
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
 
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum > 0) {
+                right -= 1;
+                continue;
+            }
+
+            if (sum < 0) {
+                left += 1;
+                continue;
+            }
+
+            const key = [nums[i], nums[left], nums[right]].join(',');
             obj[key] = true;
-        }
-
-        if (left === nums.length - 2) {
-            main += 1;
-            left = main + 1;
-            right = left + 1;
-            continue;
-        }
-
-        if (right === nums.length - 1) {
             left += 1;
-            right = left + 1;
-            continue;
         }
-
-        right += 1;
     }
 
     return Object.keys(obj).map((key) => key.split(','));
 };
-// threeSum([-1,0,1,2,-1,-4]);  //Output: [[-1,-1,2],[-1,0,1]]
-threeSum([0,0,0]);  //Output: [[0,0,0]]
+threeSum([-1,0,1,2,-1,-4]);  //Output: [[-1,-1,2],[-1,0,1]]
+// threeSum([0,0,0]);  //Output: [[0,0,0]]

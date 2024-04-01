d2e227fbae32a7704825aa78bba396feacde747f
928m <jihee9289@gmail.com>
2024-04-01 22:55:39 +0900

    Time: 37 ms (99.01%), Space: 48.8 MB (70.26%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -3,20 +3,12 @@
  * @return {number}
  */
 var findMin = function(nums) {
-  let i = 0;
-
-  if (nums.length === 1) {
-    return nums[i];
+  for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > nums[i + 1]) {
+            return nums[i + 1];
+        }
   }
 
-  while (i < nums.length) {
-    if (nums[i] < nums[i + 1]) {
-        const firstValue = nums.shift();
-        nums.push(firstValue);
-        continue;
-    }
-
-    return nums[i + 1];
-  }
+  return nums[0];
 };
 findMin([3,4,5,1,2]);
